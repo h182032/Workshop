1ef1457dd700de5ad6aa0cb20f6e28f7de9f23a2
h182032 <h182032@stud.hib.no>
2019-01-30 18:43:40 +0100

    Updates

---
+++ precache-manifest.9056034ae3ca5d3f0175010fbb2ea3e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Workshop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "40d3dea54639b7aaa176",
+    "url": "/Workshop/static/js/runtime~main.40d3dea5.js"
+  },
+  {
+    "revision": "1c0fcb219dd3ab2e6caf",
+    "url": "/Workshop/static/js/main.1c0fcb21.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Workshop/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1c0fcb219dd3ab2e6caf",
+    "url": "/Workshop/static/css/main.b4e6dcc5.chunk.css"
+  },
+  {
+    "revision": "c0ee95f86f83cc449512174db075fe11",
+    "url": "/Workshop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/workshop/precache-manifest.8149b8414583499b4296ab68f7da5cec.js"
+  "/Workshop/precache-manifest.9056034ae3ca5d3f0175010fbb2ea3e2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/workshop/index.html", {
+workbox.routing.registerNavigationRoute("/Workshop/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
